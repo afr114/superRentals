8f0127965f5f10eebf1af3045afd6ea51e01a5d8
alyssa horrocks & abdul rahimi <student@epicodus.com>
2015-09-21 16:12:50 -0700

    add front end styling, save and delete button

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
+  },
+
+  actions: {
+    save2(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+  },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
